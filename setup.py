db4306703e342b50a02d027a9c46def2276659c9
Vijayaragavan46 <vijayaragavan46@github.com>
2026-04-27 15:46:29 +0530

    chore: bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="smart-commit-generator",
-    version="1.0.0",
+    version="1.1.0",
     author="Vijayaragavan46",
     author_email="",
     description="AI-powered Git commit message generator using Claude",
